3c2f6b86263116a954aa758bde4739090e3e7220
Matt Wildoer <mattwildoer@Matts-MacBook-Air.local>
2022-07-31 16:38:28 -0700

    Update Project Homepage

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='dualsense',
     author='Matthew Wildoer',
     author_email='mawildoer@gmail.com',
-    version='0.0.1',
+    version='0.0.2',
     py_modules=['dualsense',],
     license='MIT',
     long_description=open('README.md').read(),
